84ece8f9deb84c4b600113a0c4e1495f77394074
adamspersonaldeveloping <adamspersonaldeveloping@gmail.com>
2022-09-28 12:23:19 +0800

    added a forum

---
+++ controllers/forumTopics.js
@@ -0,0 +1,60 @@
+const cloudinary = require("../middleware/cloudinary");
+const Forum = require("../models/Forum");
+const Post = require("../models/Post")
+
+
+module.exports = {
+ 
+
+  getForumFeed: async (req, res) => {
+    try {
+      const forumPosts = await Forum.find().sort({ createdAt: "desc" }).lean();
+      res.render("forum.ejs", { ForumPosts: forumPosts });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+ 
+  getForumPost: async (req, res) => {
+    try {
+      const forumPost = await Forum.findById(req.params.id);
+      res.render("forumPost.ejs", { forumPost: forumPost, user: req.user });
+      //console.log(post)
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createForumPost: async (req, res) => {
+    console.log(req.body)
+    try {
+      
+      await Forum.create({
+        title: req.body.title,
+        message: req.body.message,
+        userName: req.user.userName,
+        user: req.user.id,
+      });
+      console.log(req.file)
+      console.log("Forum post has been added!");
+      res.redirect("/forumFeed");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+ 
+ 
+  deleteForumPost: async (req, res) => {
+    try {
+      // Find post by id
+      //let forumPost = await Forum.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+     // await cloudinary.uploader.destroy(forumPost.cloudinaryId);
+      // Delete post from db
+      await Forum.remove({ _id: req.params.id });
+      console.log("Deleted Forum Post");
+      res.redirect("/forumFeed");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};
--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const forumController = require("../controllers/forumTopics")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -11,6 +12,7 @@ router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/favorites", ensureAuth, postsController.getFavorites);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/feedZtoA", ensureAuth, postsController.getFeedZtoA);
+router.get("/forumFeed", ensureAuth, forumController.getForumFeed);
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
